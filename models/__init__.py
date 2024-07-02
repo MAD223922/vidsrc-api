cf5b22f95d7dbcb5c6f1379c97f7a838d15e4198
MAD223922 <madgamer1110@gmail.com>
2024-07-02 19:39:21 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .subtitle import subfetch
 VERSION = '3.0.0'
-from .vidsrcme import get as vidsrcmeget
+from .vidsrcto import get as vidsrcmeget
 from .vidsrcto import get as vidsrctoget
 from .utils import fetch
 # UTILS
